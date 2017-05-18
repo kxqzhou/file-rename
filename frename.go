04f55b515ee985edfada92e1a77bc2e5d4af25c8
Kyle <skz.zxc@gmail.com>
2017-05-18 17:04:13 -0400

    setting up cl input

--- frename.go
+++ frename.go
@@ -5,15 +5,40 @@ import (
 	"fmt"
 	"os"
 	"log"
+	"flag"
+	"string"
 	"strconv"
-	//"flag"
 	"io/ioutil"
 )
 
-func main() {
+type replFlag []string
 
-	dir := "."
-	
+func (i *replFlag) String() string {
+	return i[0] + " " + i[1]
+}
+
+func(i *replFlag) Set(value string) error {
+	args := string.Split( value, " " )
+	i[0] = args[0]
+	i[1] = args[1]
+	return nil
+}
+
+func main() {
+	groupCommand := flag.NewFlagSet( "group", flag.ExitOnError )
+	seqCommand := flag.NewFlagSet( "seq", flag.ExitOnError )
+
+	groupDir := group.String( "folder", ".", "The folder containing the files to be renamed" )
+	prefix := group.String( "prefix", "", "A prefix to be appended to the front of each file name" )
+	postfix := group.String( "postfix", "", "A prefix to be appended to the back of each file name" )
+	var swap replFlag
+	group.Var( &swap "", "A substring and its replacement value, separated by a space" )
+
+	seqDir := seq.String( "folder", ".", "The folder containing the files to be renamed" )
+	name := seq.String( "base", "", "The shared base name among all files in the sequence" )
+
+	flag.Parse()
+
 	files, err := ioutil.ReadDir( dir )
 	if err != nil {
 		log.Fatal( err )
